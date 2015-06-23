40407418dd0429ba7b61c9d354691c8e2795b0e0
Diyah <me@hadiyah.me>
2015-06-22 21:19:26 -0700

    update exercise solution

--- js-prototypes/solution.js
+++ js-prototypes/solution.js
@@ -14,3 +14,21 @@ Triangle.prototype = shape;
 Triangle.prototype.getPerimeter = function() {
   return this.a + this.b + this.c;
 }
+
+//Other way of doing this
+function shape() {
+  this.type = '';
+  this.getType = function (){ return this.type; }
+}
+function Triangle(a, b, c) {
+  this.type = 'triangle';
+  this.a = a;
+  this.b = b;
+  this.c = c;
+}
+
+Triangle.prototype = new shape();
+
+Triangle.prototype.getPerimeter = function() {
+  return this.a + this.b + this.c;
+}
